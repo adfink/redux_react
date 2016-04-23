a5d2c44c981e0d2f69385d0e1fed5594c6128d13
Andrew Fink <andrewfink8@gmail.com>
2016-04-22 19:17:58 -0700

    getting started with reducers in redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+ export default function() {
+   return [
+     { title: "The Sword in the Stone"},
+     { title: "Wilderness and the American Mind"},
+     { title: "Walden"},
+     { title: "My First Summer in the Sierra"}
+   ]
+ }
